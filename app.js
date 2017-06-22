2dba3cb20c837cf386cdfe039834c1c0fa3860d5
Ian Waites <ian.waites@gmail.com>
2017-06-22 10:23:40 -0700

    removed old code

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// The thing you want to build today will select three random photos from the image directory and display them side-by-side-by-side in the browser window.
-
-// - In addition, you'll want to be able to receive clicks on those displayed images, and track those clicks for each image. You'll also want to track how many times each image is displayed, for statistical purposes.
-
-// - Upon receiving a click, three new non-duplicating random images need to be automatically displayed. In other words, the three images that are displayed should contain no duplicates, nor should they duplicate with any images that we displayed immediately before.
-
-// - To do this, you'll want a constructor function that creates an object associated with each image, and has (at a minimum) properties for the name of the image (to be used for display purposes), its filepath, the number of times it has been shown, and the number of times it has been clicked. You'll probably find it useful to create a property that contains a text string you can use as an ID in HTML.
-
-// - After 25 selections have been made, turn off the event listeners on the images (to prevent additional voting) and also display a list of the products with votes received with each list item looking like "3 votes for the Banana Slicer".
-
 var images = document.getElementById('container');
 
 var allImages = [];
@@ -33,35 +23,6 @@ function ImageObj(name, path) {
   this.views = 0;
   allImages.push(this);
 }
-
-// Creates random number
-
-// creating another array with 3 different images
-// function getArray() {
-//   getRandomNumber();
-//   for (var i = 0; i < queImages.length; i++) {
-//     var x = queImages[i];
-//     moreImages.push(queImages[x]);
-//   }
-// };
-
-// function checkImages(){
-//   getRandomNumber();
-//   getArray();
-//   console.table(moreImages);
-//   var imageOne = queImages[0];
-//   var imageTwo = queImages[1];
-//   var imageThree = queImages[3];
-//   while(imageOne === imageTwo || imageOne === imageThree){
-//     imageOne = moreImages[0];
-//   }
-//   while(imageTwo === imageOne || imageTwo === imageThree){
-//     imageTwo = moreImages[1];
-//   }
-//   while(imageThree === imageOne || imageThree === imageTwo){
-//     imageThree = moreImages[2];
-//   }
-// }
 
 function checkPics (array, value) {
   for (var i = 0; i < allImages.length; i++) {
